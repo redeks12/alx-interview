8df6eaa033918d38ed772c8f1a036a9eb1918122
kumdan job <redeks123456@gmail.com>
2024-01-09 22:38:30 +0100

    interview questions

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""You have n number of locked boxes in front of you. 
+Each box is numbered sequentially from 0 to 
+n - 1 and each box may contain keys to the other boxes
+."""
+
+
+def pascal_triangle(n):
+    tri = [[1], [1, 1]]
+    if n <= 0:
+        return []
+
+    for i in range(2, n):
+        tri.append([1])
+        it = tri[i - 1]
+
+        for j in range(1, len(it)):
+            tri[i].append(it[j] + it[j - 1])
+        tri[i].append(1)
+
+    return tri
