e15bc6c452e9201ed52b6694b972460936d34dae
kumdan job <redeks123456@gmail.com>
2024-01-09 22:45:46 +0100

    interview questions

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,6 +6,10 @@ n - 1 and each box may contain keys to the other boxes
 
 
 def pascal_triangle(n):
+    """Create a function def pascal_triangle(n):
+    that returns a list of lists of integers representing the Pascal’s triangle of n:
+    ."""
+
     tri = [[1], [1, 1]]
     if n <= 0:
         return []
