62dbe8f1c03a45156105bc89db0600b00eaf9e8c
kumdan job <redeks123456@gmail.com>
2024-01-19 18:03:44 +0100

    interview

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,34 +2,16 @@
 """0x02. Minimum Operations"""
 
 
-def minOperations(n: int) -> int:
-    """0. Minimum Operations"""
-    valueInFile = 1
-    if n % 2 == 0:
-        allOperations = 0
-    else:
-        allOperations = 1
-    copyVal = 1
-    pasteVal = 1
-    copy = 0
-    paste = 0
-
-    # while True:
-    #     if not valueInFile % 2 == 0:
-    while True:
-        if n % 2 == 0:
-            if valueInFile * 2 <= n:
-                copyVal = valueInFile
-                pasteVal = copyVal
-                valueInFile += pasteVal
-                allOperations += 2
-            else:
-                valueInFile += pasteVal
-                allOperations += 1
-        else:
-            valueInFile += pasteVal
-            allOperations += 1
-
-        if valueInFile == n:
-            break
-    return allOperations
+def minOperations(n):
+    """
+    Function minOperations
+    Returns an integer
+    """
+    result = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            result += x
+            n /= x
+        x += 1
+    return result
