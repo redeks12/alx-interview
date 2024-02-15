f3c4847c5521dd2b2ea73000d063335a6fa8d252
kumdan job <redeks123456@gmail.com>
2024-02-15 21:59:10 +0100

    alx interview

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
-import fetch from "node-fetch";
+import request from "request";
+import { promisify } from "util";
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const asyncRequest = promisify(request);
 
 async function print_characters(urls) {
   for (const urlt of urls) {
-    const val = await fetch(urlt);
-    const data = await val.json();
-    console.log(data.name);
+    const val = await asyncRequest({ url: urlt, json: true });
+    console.log(val.body.name);
   }
 }
 
 async function get_stuff() {
-  const val = await fetch(url);
-  const data = await val.json();
-  await print_characters(data.characters);
+  const val = await asyncRequest({ url: url, json: true });
+  await print_characters(val.body.characters);
 }
 get_stuff();
