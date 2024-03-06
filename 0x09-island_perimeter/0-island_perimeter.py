ba5242cf2fdc573f64249e815bc974704291e791
kumdan job <redeks123456@gmail.com>
2024-03-06 13:48:48 +0100

    m

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""0x09. Island Perimeter"""
+# from typing import List
+
+
+def island_perimeter(grid: list[list[int]]):
+    """island_perimeter function"""
+    count = 0
+    for index, cell in enumerate(grid):
+        for ind, sq in enumerate(cell):
+            if sq == 1:
+                if cell[ind - 1] == 0:
+                    if not ind - 1 < 0:
+                        count += 1
+                if cell[ind + 1] == 0:
+                    if not len(cell) - 1 < ind + 1:
+                        count += 1
+                if grid[index - 1][ind] == 0:
+                    if not index - 1 < 0:
+                        count += 1
+                if grid[index + 1][ind] == 0:
+                    if not len(grid) - 1 < index + 1:
+                        count += 1
+
+    return count
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,3 @@
+# 0x09. Island Perimeter
+Tasks
+### `0. Island Perimeter`
