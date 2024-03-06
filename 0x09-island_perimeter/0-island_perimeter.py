5c4743367057b937dceeaf63ec81ad52efb0f1d7
kumdan job <redeks123456@gmail.com>
2024-03-06 13:53:09 +0100

    m

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,7 +3,12 @@
 
 
 def island_perimeter(grid: list[list[int]]) -> int:
-    """island_perimeter function"""
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid."""
     count = 0
     for index, cell in enumerate(grid):
         for ind, sq in enumerate(cell):
